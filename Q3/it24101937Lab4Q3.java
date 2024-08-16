2ee6fd00e9abb444974f89545b82acf323c6f221
IT24101937 <it24101937@my.sliit.lk>
2024-08-16 10:24:12 +0530

    it24101937Lab4Q3

---
+++ Q3/it24101937Lab4Q3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+    public class it24101937Lab4Q3 {
+        public static void main (String [] args) {
+            Scanner keyboard = new Scanner (System.in);
+            
+            System.out.println ("Enter the number you want to compare: ");
+            int compareNumber = keyboard.nextInt();
+            
+            
+            String g = compareNumber < 0 ?  "The number you entered is negative" :(compareNumber > 0)? "The number you entered is positive" : "the number would be zero" ;
+            System.out.println (g);
+            
+        
+            
+            
+
+            
+        }
+    }
